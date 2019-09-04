6261c15f9b72c2713d6890f614c40fdafa1c7eed
Martin Spielmann <mail@martinspielmann.de>
2019-09-04 11:41:00 +0200

    simplified error handling, introduce hibp-api-key

--- src/main/java/de/martinspielmann/wicket/pwnedpasswordsvalidator/Status.java
+++ src/main/java/de/martinspielmann/wicket/pwnedpasswordsvalidator/Status.java
@@ -10,30 +10,31 @@ import java.util.Map;
  */
 public enum Status {
 
-    UNKNOWN_API_ERROR(-1),
-    PASSWORD_PWNED(200),
-    PASSWORD_UNAUTHORIZED(401),
-    PASSWORD_OK(404),
-    TOO_MANY_REQUESTS(429);
+  UNKNOWN_API_ERROR(-1), PASSWORD_PWNED(200), UNAUTHORIZED(401), PASSWORD_OK(
+      404), TOO_MANY_REQUESTS(429);
 
-    private int code;
-    private static final Map<Integer, Status> map = new HashMap<>();
+  private int code;
+  private static final Map<Integer, Status> map = new HashMap<>();
 
-    static {
-        for (Status s : Status.values()){
-            map.put(s.code, s);
-        }
+  static {
+    for (Status s : Status.values()) {
+      map.put(s.code, s);
     }
+  }
 
-    Status(int code){
-        this.code = code;
-    }
+  Status(int code) {
+    this.code = code;
+  }
 
-    public static Status of(int code){
-        Status s = map.get(code);
-        if(s == null){
-            s = UNKNOWN_API_ERROR;
-        }
-        return s;
+  public int getCode() {
+    return code;
+  }
+
+  public static Status of(int code) {
+    Status s = map.get(code);
+    if (s == null) {
+      s = UNKNOWN_API_ERROR;
     }
+    return s;
+  }
 }
--- src/test/java/de/martinspielmann/wicket/pwnedpasswordsvalidator/HomePage.java
+++ src/test/java/de/martinspielmann/wicket/pwnedpasswordsvalidator/HomePage.java
@@ -9,19 +9,16 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
 
 public class HomePage extends WebPage {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	public HomePage(final PageParameters parameters) {
-		super(parameters);
+  public HomePage(final PageParameters parameters) {
+    super(parameters);
+    add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+    // TODO Add your page's components here
+    Form<Void> f = new Form<>("form");
+    add(f);
+    f.add(new FeedbackPanel("feedback"));
+    f.add(new PasswordTextField("pw", new Model<>("")).add(new PwnedPasswordsValidator("foo")));
 
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
-		// TODO Add your page's components here
-
-		Form f = new Form("form");
-		add(f);
-		f.add(new FeedbackPanel("feedback"));
-		f.add(new PasswordTextField("pw", new Model<>("")).add(new PwnedPasswordsValidator()));
-
-    }
+  }
 }
