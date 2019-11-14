2d003035c60d0671c451c973a10977266142b677
Martin Spielmann <mail@martinspielmann.de>
2019-11-14 12:21:51 +0100

    update to haveibeenpwned4j

--- src/main/java/de/martinspielmann/wicket/pwnedpasswordsvalidator/PwnedPasswordsValidator.java
+++ src/main/java/de/martinspielmann/wicket/pwnedpasswordsvalidator/PwnedPasswordsValidator.java
@@ -1,55 +1,47 @@
 package de.martinspielmann.wicket.pwnedpasswordsvalidator;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.wicket.util.string.Strings;
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.IValidationError;
 import org.apache.wicket.validation.IValidator;
 import org.apache.wicket.validation.ValidationError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.io.IOException;
-import java.io.Serializable;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+
+import de.martinspielmann.haveibeenpwned4j.HaveIBeenPwnedApiClient;
+import de.martinspielmann.haveibeenpwned4j.HaveIBeenPwnedException;
+import java.net.InetSocketAddress;
 import java.net.Proxy;
-import java.net.SocketAddress;
-import java.net.URL;
-import java.net.Proxy.Type;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * Checks if a given password has been disclosed in a data breach using API of
- * <a href="https://haveibeenpwned.com/">https://haveibeenpwned.com/</a> More details at <a href=
+ * <a href="https://haveibeenpwned.com/">https://haveibeenpwned.com/</a> More
+ * details at <a href=
  * "https://haveibeenpwned.com/API/v3#PwnedPasswords">https://haveibeenpwned.com/API/v2#PwnedPasswords</a>
  *
  * @author Martin Spielmann
  */
 public class PwnedPasswordsValidator implements IValidator<String> {
 
-  private static final long serialVersionUID = -2295946752223344261L;
+  private static final long serialVersionUID = 1L;
 
   private static final Logger LOG = LoggerFactory.getLogger(PwnedPasswordsValidator.class);
 
-  private static final String API_URL = "https://api.pwnedpasswords.com/range/%s";
-
-  private final String apiKey;
   private final boolean failOnError;
-  private final SerializableProxy proxy;
+
+  private final HaveIBeenPwnedApiClient client;
 
   /**
-   * Creates a new PwnedPasswordsValidator with default configuration without an API key. If an error
-   * occurs during validation, the validated password will be treated as invalid.
+   * Creates a new PwnedPasswordsValidator with default configuration without an
+   * API key. If an error occurs during validation, the validated password will be
+   * treated as invalid.
    */
   public PwnedPasswordsValidator() {
     this(null, true, null);
   }
 
   /**
-   * Creates a new PwnedPasswordsValidator with default configuration. If an error occurs during
-   * validation, the validated password will be treated as invalid.
+   * Creates a new PwnedPasswordsValidator with default configuration. If an error
+   * occurs during validation, the validated password will be treated as invalid.
    * 
    * @param apiKey the hibp-api-key
    */
@@ -60,10 +52,10 @@ public class PwnedPasswordsValidator implements IValidator<String> {
   /**
    * Creates a new PwnedPasswordsValidator
    *
-   * @param apiKey the hibp-api-key
-   * @param failOnError If {@code true}, if an error occurs during validation, the validated password
-   *        will be treated as invalid. Else errors will be ignored and the password will be treated
-   *        as valid.
+   * @param apiKey      the hibp-api-key
+   * @param failOnError If {@code true}, if an error occurs during validation, the
+   *                    validated password will be treated as invalid. Else errors
+   *                    will be ignored and the password will be treated as valid.
    */
   public PwnedPasswordsValidator(String apiKey, boolean failOnError) {
     this(apiKey, failOnError, null);
@@ -72,120 +64,42 @@ public class PwnedPasswordsValidator implements IValidator<String> {
   /**
    * Creates a new PwnedPasswordsValidator.
    *
-   * @param apiKey the hibp-api-key
-   * @param failOnError If {@code true}, if an error occurs during validation, the validated password
-   *        will be treated as invalid. Else errors will be ignored and the password will be treated
-   *        as valid.
-   * @param proxy the proxy server
+   * @param apiKey      the hibp-api-key
+   * @param failOnError If {@code true}, if an error occurs during validation, the
+   *                    validated password will be treated as invalid. Else errors
+   *                    will be ignored and the password will be treated as valid.
+   * @param proxy       the proxy server
    */
   public PwnedPasswordsValidator(String apiKey, boolean failOnError, Proxy proxy) {
-    this.apiKey = apiKey;
     this.failOnError = failOnError;
-    if (proxy != null) {
-      this.proxy = new SerializableProxy(proxy);
-    } else {
-      this.proxy = null;
-    }
+    InetSocketAddress proxyAddress = proxy != null ? (InetSocketAddress) proxy.address() : null;
+    client = new HaveIBeenPwnedApiClient(apiKey, proxyAddress, "pingunaut/wicket-pwnedpasswords-validator");
+  }
+
+  protected HaveIBeenPwnedApiClient getClient() {
+    return client;
   }
 
   @Override
   public void validate(IValidatable<String> validatable) {
     String pw = validatable.getValue();
-    Status status = getResponseStatus(pw);
-    switch (status) {
-      case UNKNOWN_API_ERROR:
-      case TOO_MANY_REQUESTS:
-      case UNAUTHORIZED:
-        if (shouldFailOnError()) {
-          validatable
-              .error(decorate(new ValidationError(this, "error").setVariable("code", status.getCode()), validatable));
-        }
-        break;
-      case PASSWORD_PWNED:
-        validatable.error(decorate(new ValidationError(this, "pwned"), validatable));
-        break;
-      case PASSWORD_OK:
-        // great. password not pwned.
-        break;
-      default:
-        break;
-    }
-  }
-
-  protected Status getResponseStatus(String pw) {
     try {
-
-      HttpURLConnection c;
-      if (getProxy() != null) {
-        c = (HttpURLConnection) getApiUrl(pw).openConnection(getProxy());
-      } else {
-        c = (HttpURLConnection) getApiUrl(pw).openConnection();
+      boolean isPwned = getClient().isPasswordPwned(pw);
+      if (isPwned) {
+        validatable.error(decorate(new ValidationError(this, "pwned"), validatable));
       }
-      c.setRequestMethod("GET");
-      c.setRequestProperty("User-Agent", "pingunaut/wicket-pwnedpasswords-validator");
-      if (apiKey != null) {
-        c.setRequestProperty("hibp-api-key", apiKey);
+    } catch (HaveIBeenPwnedException e) {
+      if (shouldFailOnError()) {
+        validatable
+            .error(decorate(new ValidationError(this, "error").setVariable("code", e.getMessage()), validatable));
       }
-      c.connect();
-
-      Status status = Status.of(c.getResponseCode());
-      // if nothing is found or there was an API error, return
-      if (!status.equals(Status.PASSWORD_PWNED)) {
-        return status;
-      }
-      // if there were results, check if your pw hash was pwned
-      String result = IOUtils.toString(c.getInputStream(), StandardCharsets.UTF_8);
-      String[] lines = result.split("\\r?\\n");
-      String hashSuffix = getHashSuffix(pw);
-      for (String line : lines) {
-        if (line.split(":")[0].equals(hashSuffix)) {
-          return Status.PASSWORD_PWNED;
-        }
-      }
-      return Status.PASSWORD_OK;
-    } catch (IOException | NoSuchAlgorithmException e) {
-      LOG.error("Error checking password for pwnage", e);
-      return Status.UNKNOWN_API_ERROR;
     }
-  }
-
-  protected Proxy getProxy() {
-    if (proxy == null) {
-      return null;
-    }
-    return proxy.get();
-  }
-
-  String getHashPrefix(String pw) throws NoSuchAlgorithmException {
-    return sha1(pw).substring(0, 5);
-  }
-
-  String getHashSuffix(String pw) throws NoSuchAlgorithmException {
-    return sha1(pw).substring(5);
-  }
-
-  URL getApiUrl(String pw) throws MalformedURLException, NoSuchAlgorithmException {
-    return new URL(String.format(API_URL, getHashPrefix(pw)));
-  }
-
-  /**
-   * The API allows to check passwords directly. To avoid url encoding issues with complicated
-   * passwords, we use the possibility to check sha1 hashes
-   *
-   * @param pw the password
-   * @return sha1 hash of the given password
-   * @throws NoSuchAlgorithmException if SHA-1 digest not available
-   */
-  protected String sha1(String pw) throws NoSuchAlgorithmException {
-    MessageDigest digest = MessageDigest.getInstance("SHA-1");
-    byte[] encodedhash = digest.digest(pw.getBytes(StandardCharsets.UTF_8));
-    return Strings.toHexString(encodedhash);
   }
 
   /**
    * Allows subclasses to decorate reported errors
    *
-   * @param error the error
+   * @param error       the error
    * @param validatable the validatable
    * @return decorated error
    */
@@ -196,26 +110,4 @@ public class PwnedPasswordsValidator implements IValidator<String> {
   boolean shouldFailOnError() {
     return failOnError;
   }
-
-
-  private static class SerializableProxy implements Serializable {
-    private static final long serialVersionUID = 6611963348855847317L;
-
-    private Type type;
-    private SocketAddress sa;
-
-    public SerializableProxy(Proxy proxy) {
-      this.type = proxy.type();
-      this.sa = proxy.address();
-    }
-
-    public Proxy get() {
-      if (type == Type.DIRECT) {
-        return Proxy.NO_PROXY;
-      }
-      return new Proxy(type, sa);
-    }
-
-  }
-
 }
--- src/test/java/de/martinspielmann/wicket/pwnedpasswordsvalidator/PwnedPasswordsValidatorTest.java
+++ src/test/java/de/martinspielmann/wicket/pwnedpasswordsvalidator/PwnedPasswordsValidatorTest.java
@@ -5,12 +5,12 @@ import org.apache.wicket.util.tester.WicketTester;
 import org.apache.wicket.validation.Validatable;
 import org.junit.Assert;
 import org.junit.Test;
-import java.net.*;
-import java.security.NoSuchAlgorithmException;
-import java.util.UUID;
+
+import de.martinspielmann.haveibeenpwned4j.HaveIBeenPwnedApiClient;
+import de.martinspielmann.haveibeenpwned4j.HaveIBeenPwnedException;
+import de.martinspielmann.haveibeenpwned4j.Status;
 
 public class PwnedPasswordsValidatorTest {
-
 
   private String apiKey = "foo123";
 
@@ -18,37 +18,27 @@ public class PwnedPasswordsValidatorTest {
   public void constructorsShouldSetDefaultValues() {
     PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey);
     Assert.assertEquals(true, v.shouldFailOnError());
-    Assert.assertNull(v.getProxy());
   }
 
   @Test
   public void constructorsShouldSetParameterValues() {
     PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey, false);
     Assert.assertEquals(false, v.shouldFailOnError());
-    Assert.assertNull(v.getProxy());
-  }
-
-  @Test
-  public void constructorsShouldSetAllParameterValuesValues() {
-    Proxy proxy = Proxy.NO_PROXY;
-    PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey, false, proxy);
-    Assert.assertEquals(false, v.shouldFailOnError());
-    Assert.assertEquals(Proxy.NO_PROXY, v.getProxy());
-  }
-
-  @Test
-  public void testSha1() throws NoSuchAlgorithmException {
-    PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey);
-    String sha1 = v.sha1("foo");
-    Assert.assertEquals("0BEEC7B5EA3F0FDBC95D0DD47F3C5BC275DA8A33", sha1);
   }
 
   @Test
   public void validateUnknownError() {
     PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey) {
+      private static final long serialVersionUID = 1L;
+
       @Override
-      protected Status getResponseStatus(String pw) {
-        return Status.UNKNOWN_API_ERROR;
+      protected HaveIBeenPwnedApiClient getClient() {
+        return new HaveIBeenPwnedApiClient() {
+          @Override
+          public boolean isPasswordPwned(String password) {
+            throw new HaveIBeenPwnedException("Foo!", Status.of(503));
+          }
+        };
       }
     };
     Validatable<String> validatable = new Validatable<>();
@@ -59,9 +49,16 @@ public class PwnedPasswordsValidatorTest {
   @Test
   public void validateUnknownErrorIgnore() {
     PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey, false) {
+      private static final long serialVersionUID = 1L;
+
       @Override
-      protected Status getResponseStatus(String pw) {
-        return Status.UNKNOWN_API_ERROR;
+      protected HaveIBeenPwnedApiClient getClient() {
+        return new HaveIBeenPwnedApiClient() {
+          @Override
+          public boolean isPasswordPwned(String password) {
+            throw new HaveIBeenPwnedException("Foo!", Status.of(503));
+          }
+        };
       }
     };
     Validatable<String> validatable = new Validatable<>();
@@ -72,9 +69,16 @@ public class PwnedPasswordsValidatorTest {
   @Test
   public void validatePwned() {
     PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey) {
+      private static final long serialVersionUID = 1L;
+
       @Override
-      protected Status getResponseStatus(String pw) {
-        return Status.PASSWORD_PWNED;
+      protected HaveIBeenPwnedApiClient getClient() {
+        return new HaveIBeenPwnedApiClient() {
+          @Override
+          public boolean isPasswordPwned(String password) {
+            return true;
+          }
+        };
       }
     };
     Validatable<String> validatable = new Validatable<>();
@@ -85,9 +89,16 @@ public class PwnedPasswordsValidatorTest {
   @Test
   public void validatePwOK() {
     PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey) {
+      private static final long serialVersionUID = 1L;
+
       @Override
-      protected Status getResponseStatus(String pw) {
-        return Status.PASSWORD_OK;
+      protected HaveIBeenPwnedApiClient getClient() {
+        return new HaveIBeenPwnedApiClient() {
+          @Override
+          public boolean isPasswordPwned(String password) {
+            return false;
+          }
+        };
       }
     };
     Validatable<String> validatable = new Validatable<>();
@@ -95,40 +106,10 @@ public class PwnedPasswordsValidatorTest {
     Assert.assertEquals(0, validatable.getErrors().size());
   }
 
-
-  @Test
-  public void getResponseStatusPwned() {
-    PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey);
-    Status s = v.getResponseStatus("secret123");
-    Assert.assertEquals(Status.PASSWORD_PWNED, s);
-  }
-
-  @Test
-  public void getResponseStatusForRandomPassword() {
-    PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey);
-    Status s = v.getResponseStatus(UUID.randomUUID().toString() + UUID.randomUUID().toString());
-    Assert.assertEquals(Status.PASSWORD_OK, s);
-  }
-
-  @Test
-  public void getProxy() {
-    SocketAddress sa = new InetSocketAddress(80);
-    Proxy p = new Proxy(Proxy.Type.HTTP, sa);
-    PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey, false, p);
-    Assert.assertEquals(p, v.getProxy());
-  }
-
   @Test
   public void shouldFailOnError() {
     PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey, false);
     Assert.assertFalse(v.shouldFailOnError());
-  }
-
-  @Test
-  public void getApiUrl() throws MalformedURLException, NoSuchAlgorithmException {
-    PwnedPasswordsValidator v = new PwnedPasswordsValidator(apiKey);
-    URL u = new URL("https://api.pwnedpasswords.com/range/F2B14");
-    Assert.assertEquals(u, v.getApiUrl("secret123"));
   }
 
   @Test
@@ -145,8 +126,7 @@ public class PwnedPasswordsValidatorTest {
     FormTester ft = tester.newFormTester("form");
     ft.setValue("pw", "secret123");
     ft.submit();
-    String feedback = ft.getForm().getLocalizer().getString("PwnedPasswordsValidator.pwned",
-        ft.getForm().get("pw"));
+    String feedback = ft.getForm().getLocalizer().getString("PwnedPasswordsValidator.pwned", ft.getForm().get("pw"));
     tester.assertErrorMessages(feedback);
   }
 }
